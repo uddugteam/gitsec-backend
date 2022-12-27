4063b8ac5df5f999df9e8e7d29d8f4d2986399d9
Andrew Skurlatov <a.skurlatov@gmail.com>
2022-12-27 22:05:40 +0100

    fix: typo in comments

--- internal/application.go
+++ internal/application.go
@@ -87,7 +87,7 @@ func (app *App) Version() string {
 	return app.version.String()
 }
 
-// CreateAddr is create address string from host and port
+// CreateAddr is created address string from host and port
 func CreateAddr(host string, port int) string {
 	return fmt.Sprintf("%s:%v", host, port)
 }
